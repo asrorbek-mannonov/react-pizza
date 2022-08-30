6762a05a7903dab93d55e284ecb2c394105e818f
Asrorbek Mannonov <44311347+asrorbek-mannonov@users.noreply.github.com>
2022-08-30 09:58:06 +0500

    fix: styling

--- src/components/PizzaCard/Skeleton.jsx
+++ src/components/PizzaCard/Skeleton.jsx
@@ -1,24 +1,32 @@
-import React from "react"
-import ContentLoader from "react-content-loader"
+import React from 'react';
+import ContentLoader from 'react-content-loader';
 
-const Skeleton = (props) => (
-  <ContentLoader 
-    speed={2}
-    width={280}
-    height={457}
-    viewBox="0 0 280 457"
-    backgroundColor="#f3f3f3"
-    foregroundColor="#ecebeb"
-    {...props}
-  >
-    <circle cx="138" cy="138" r="138" /> 
-    <rect x="150" y="305" rx="0" ry="0" width="13" height="0" /> 
-    <rect x="0" y="287" rx="10" ry="10" width="276" height="24" /> 
-    <rect x="2" y="320" rx="10" ry="10" width="270" height="84" /> 
-    <rect x="2" y="422" rx="10" ry="10" width="90" height="27" /> 
-    <rect x="124" y="411" rx="10" ry="10" width="150" height="44" />
-  </ContentLoader>
-)
+const Skeleton = props => (
+  <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <ContentLoader
+      speed={2}
+      width={280}
+      height={457}
+      viewBox="0 0 280 457"
+      backgroundColor="#f3f3f3"
+      foregroundColor="#ecebeb"
+      {...props}
+    >
+      <circle cx="138" cy="138" r="138" />
+      <rect x="150" y="305" rx="0" ry="0" width="13" height="0" />
+      <rect x="0" y="287" rx="10" ry="10" width="276" height="24" />
+      <rect x="2" y="320" rx="10" ry="10" width="270" height="84" />
+      <rect x="2" y="422" rx="10" ry="10" width="90" height="27" />
+      <rect
+        x="124"
+        y="411"
+        rx="10"
+        ry="10"
+        width="150"
+        height="44"
+      />
+    </ContentLoader>
+  </div>
+);
 
-export default Skeleton
-
+export default Skeleton;
